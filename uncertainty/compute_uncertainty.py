855d448be959c3acf91ab6954023ac81abd312dc
aaronk2002 <aaronakj2002@gmail.com>
2026-01-04 16:53:09 -0500

    Removed LL method

--- uncertainty/compute_uncertainty.py
+++ uncertainty/compute_uncertainty.py
@@ -15,32 +15,10 @@ def compute_dist_uncertainty(X, Y, k=100, metric='cosine'):
     knn_dist, _ = torch.topk(distances, k=k, largest=False, dim=1)
     return knn_dist.mean(dim=1)
 
-def compute_knn_idxs(X, Y, k=100, metric='cosine'):
-    distances = compute_distance(X, Y, metric=metric).squeeze(0)
-    _, knn_idxs = torch.topk(distances, k=k, largest=False, dim=1)
-    return [set(idxs.tolist()) for idxs in knn_idxs]
-
 def compute_fv_uncertainty(Ys, metric='cosine'):
     if metric == "cosine":
         Ys = [Y / Y.norm(dim=1, keepdim=True) for Y in Ys]
     return torch.var(torch.stack(Ys), dim=0, correction=0).sum(dim=-1)
-
-def compute_ll_uncertainty(X, Y, n_clusters=20, max_iters=30, metric='cosine'):
-    kernel = (
-        VonMisesFisherMixture(
-            n_clusters=n_clusters,
-            posterior_type='soft',
-            max_iter=max_iters,
-            normalize=False
-        ) if metric == "cosine" else
-        GaussianMixture(n_components=n_clusters, max_iter=max_iters)
-    )
-    kernel.fit(X.cpu().numpy())
-    return (
-        kernel.predict_log_proba(Y.cpu().numpy())
-        if metric == "cosine" else
-        kernel.score_samples(Y.cpu().numpy())
-    )
 
 def compute_nc_uncertainty(Xs, Ys, k=100, metric='cosine'):
     N = len(Xs)
