c05916ed6a419567515100b48754541ff75dbad5
aaronk2002 <aaronakj2002@gmail.com>
2026-01-04 16:42:36 -0500

    Removed automatic stddev tuning

--- main_perturb.py
+++ main_perturb.py
@@ -6,7 +6,7 @@ import torch
 
 from model_dataset import get_model, get_dataset, save_model
 from utils import perturb, get_simclr_loss, get_byol_loss, get_moco_loss
-from stddev_tuning import tuned_perturb
+# from stddev_tuning import tuned_perturb
 
 
 LOSS_FN = {
@@ -30,7 +30,7 @@ def get_args_parser():
     # Synthetic ensemble configutations
     parser.add_argument('--method', type=str)
     parser.add_argument('--stddev', type=float, default=None)
-    parser.add_argument('--stddev-list', type=str, default=None)
+    # parser.add_argument('--stddev-list', type=str, default=None)
     parser.add_argument('--inputs', type=str, default=None)
     parser.add_argument('--cache', type=str, default=None)
     parser.add_argument('--overwrite', action='store_true')
@@ -79,34 +79,35 @@ def main(args):
 
     # Otherwise, tune standard deviation
     else:
+        raise Exception("Must have standard deviation")
         # Training set for finding standard deviation with the best spread
-        trainset = get_dataset(
-            args.ssl, args.pretrain, [(args.pretrain, True)]
-        )[args.pretrain, True][0].to(device)
-        # Search space of standard deviations
-        stddevs = None
-        if args.stddev_list is not None:
-            with open(args.stddev_list, 'r') as f:
-                stddevs = json.load(f)
-        # Get ensemble
-        models, stddev_dict = tuned_perturb(
-            embedding_model,
-            args.n_ens,
-            args.method,
-            trainset,
-            device,
-            stddevs=stddevs,
-            loss_fn=LOSS_FN[args.ssl],
-            pert_inputs=pert_inputs,
-            cache=args.cache,
-            overwrite=args.overwrite,
-            k=args.k,
-            metric=args.metric,
-            generator=generator
-        )
-        for idx, model in enumerate(models):
-            save_model(model, f"{args.outdir}/{idx}.pth", args.ssl, args.arch)
-        torch.save(stddev_dict, f"{args.outdir}/stddev_dict.pth")
+        # trainset = get_dataset(
+        #     args.ssl, args.pretrain, [(args.pretrain, True)]
+        # )[args.pretrain, True][0].to(device)
+        # # Search space of standard deviations
+        # stddevs = None
+        # if args.stddev_list is not None:
+        #     with open(args.stddev_list, 'r') as f:
+        #         stddevs = json.load(f)
+        # # Get ensemble
+        # models, stddev_dict = tuned_perturb(
+        #     embedding_model,
+        #     args.n_ens,
+        #     args.method,
+        #     trainset,
+        #     device,
+        #     stddevs=stddevs,
+        #     loss_fn=LOSS_FN[args.ssl],
+        #     pert_inputs=pert_inputs,
+        #     cache=args.cache,
+        #     overwrite=args.overwrite,
+        #     k=args.k,
+        #     metric=args.metric,
+        #     generator=generator
+        # )
+        # for idx, model in enumerate(models):
+        #     save_model(model, f"{args.outdir}/{idx}.pth", args.ssl, args.arch)
+        # torch.save(stddev_dict, f"{args.outdir}/stddev_dict.pth")
 
 
 
