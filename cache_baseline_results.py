9fac31c89e95099af434ddbd5bcbc9565b1e6a91
aaronk2002 <aaronakj2002@gmail.com>
2026-01-04 16:42:56 -0500

    Changed cache scripts

--- cache_baseline_results.py
+++ cache_baseline_results.py
@@ -6,34 +6,56 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from datasets import load_dataset, load_from_disk
 
-ALL_STDDEVS = ["1e-05", "1.778e-05", "3.162e-05", "5.623e-05", "0.0001", "0.00017783", "0.00031623", "0.00056234", "0.001", "0.00177828", "0.00316228", "0.00562341", "0.01", "0.01778279", "0.03162278", "0.05623413", "0.1"]
-
-def cache_baselines(ssl, pretrain, arch, downstream):
-    ret = {
+def cache_baselines(ssl, pretrain, arch, downstream, downstream_task):
+    print(ssl, pretrain, arch, downstream, downstream_task)
+    ret_brier = {
         model_num: {
             method: []
-            for method in ["nc", "norm", "dist", "fv"]
+            for method in ["nc", "norm", "dist", "norm_ens", "dist_ens", "fv"]
+        }
+        for model_num in range(10)
+    }
+    ret_ent = {
+        model_num: {
+            method: []
+            for method in ["nc", "norm", "dist", "norm_ens", "dist_ens", "fv"]
         }
         for model_num in range(10)
     }
     for seed in range(5):
         baseline_uncertainty = torch.load(f"baseline_uncertainty/{ssl}_{pretrain}_{arch}_{downstream}/{seed}.pth", weights_only=False, map_location="cpu")
+        norm_unc = -baseline_uncertainty["norm"][0].clone()
+        dist_unc = baseline_uncertainty["dist"]["cosine"][1][0].clone()
+        for model_num in range(1, 10):
+            norm_unc -= baseline_uncertainty["norm"][model_num].clone()
+            dist_unc += baseline_uncertainty["dist"]["cosine"][1][model_num].clone()
+        norm_unc /= 10
+        dist_unc /= 10
         nc_baseline_uncertainty = -torch.load(f"nc_uncertainty/{ssl}_{pretrain}_{arch}_{downstream}/baseline_nc_{seed}.pth", weights_only=False, map_location="cpu")
         for model_num in range(10):
-            downstream_performance = torch.load(f"downstream/{ssl}_{pretrain}_{arch}_{model_num}_{downstream}_binary_test.pth")
-            downstream_performance = downstream_performance["brier"]
-            ret[model_num]["nc"].append(kendalltau(downstream_performance, nc_baseline_uncertainty).statistic)
-            ret[model_num]["norm"].append(kendalltau(downstream_performance, baseline_uncertainty["norm"]).statistic)
-            ret[model_num]["dist"].append(kendalltau(downstream_performance, baseline_uncertainty["dist"]["cosine"][1]).statistic)
-            ret[model_num]["fv"].append(kendalltau(downstream_performance, baseline_uncertainty["fv"]["cosine"]).statistic)
-    torch.save(ret, f"result_cache/{ssl}_{pretrain}_{arch}_{downstream}_baseline_corrs.pth")
+            downstream_performance = torch.load(f"downstream/{ssl}_{pretrain}_{arch}_{model_num}_{downstream}_{downstream_task}_test.pth")
+            ret_brier[model_num]["nc"].append(kendalltau(downstream_performance["brier"], nc_baseline_uncertainty).statistic)
+            ret_brier[model_num]["norm"].append(kendalltau(downstream_performance["brier"], -baseline_uncertainty["norm"][model_num]).statistic)
+            ret_brier[model_num]["dist"].append(kendalltau(downstream_performance["brier"], baseline_uncertainty["dist"]["cosine"][1][model_num]).statistic)
+            ret_brier[model_num]["norm_ens"].append(kendalltau(downstream_performance["brier"], norm_unc).statistic)
+            ret_brier[model_num]["dist_ens"].append(kendalltau(downstream_performance["brier"], dist_unc).statistic)
+            ret_brier[model_num]["fv"].append(kendalltau(downstream_performance["brier"], baseline_uncertainty["fv"]["cosine"]).statistic)
+            ret_ent[model_num]["nc"].append(kendalltau(downstream_performance["pred_entropy"], nc_baseline_uncertainty).statistic)
+            ret_ent[model_num]["norm"].append(kendalltau(downstream_performance["pred_entropy"], -baseline_uncertainty["norm"][model_num]).statistic)
+            ret_ent[model_num]["dist"].append(kendalltau(downstream_performance["pred_entropy"], baseline_uncertainty["dist"]["cosine"][1][model_num]).statistic)
+            ret_ent[model_num]["norm_ens"].append(kendalltau(downstream_performance["pred_entropy"], norm_unc).statistic)
+            ret_ent[model_num]["dist_ens"].append(kendalltau(downstream_performance["pred_entropy"], dist_unc).statistic)
+            ret_ent[model_num]["fv"].append(kendalltau(downstream_performance["pred_entropy"], baseline_uncertainty["fv"]["cosine"]).statistic)
+    ret = {"brier": ret_brier, "pred_entropy": ret_ent}
+    torch.save(ret, f"result_cache/{ssl}_{pretrain}_{arch}_{downstream}_{downstream_task}_baseline_corrs.pth")
 
-for ssl in ["simclr", "byol", "moco"]:
-    for arch in ["resnet18", "resnet50"]:
-        for pretrain in ["cifar10", "cifar100"]:
-            downstream = pretrain
-            cache_baselines(ssl, pretrain, arch, downstream)
-        pretrain = "imagenet32"
-        for downstream in ["cifar10", "cifar100", "stl10"]:
-            cache_baselines(ssl, pretrain, arch, downstream)
+for downstream_task in ["binary", "multi"]:
+    for ssl in ["simclr", "byol", "moco"]:
+        for arch in ["resnet18", "resnet50"]:
+            for pretrain in ["cifar10", "cifar100"]:
+                downstream = pretrain
+                cache_baselines(ssl, pretrain, arch, downstream, downstream_task)
+            pretrain = "imagenet32"
+            for downstream in ["cifar10", "cifar100", "stl10"]:
+                cache_baselines(ssl, pretrain, arch, downstream, downstream_task)
     
--- cache_results.py
+++ cache_results.py
@@ -6,48 +6,27 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from datasets import load_dataset, load_from_disk
 
-ALL_STDDEVS = ["1e-05", "1.778e-05", "3.162e-05", "5.623e-05", "0.0001", "0.00017783", "0.00031623", "0.00056234", "0.001", "0.00177828", "0.00316228", "0.00562341", "0.01", "0.01778279", "0.03162278", "0.05623413", "0.1"]
+ALL_STDDEVS = ["1e-05", "1.778e-05", "3.162e-05", "5.623e-05", "0.0001", "0.00017783", "0.00031623", "0.00056234", "0.001", "0.00177828", "0.00316228", "0.00562341", "0.01", "0.01778279", "0.03162278", "0.05623413", "0.1", "0.17782794", "0.31622777", "0.56234133", "1.0"]
 
-def cache_indist_other_methods(ssl, pretrain, arch, downstream, methods):
+def cache_indist_other_methods(ssl, pretrain, arch, downstream, methods, downstream_task):
     with torch.no_grad():
         for method in methods:
             # Test correlations
-            test_correlation = {model_num: [] for model_num in range(10)}
+            test_brier_correlation = {model_num: [] for model_num in range(10)}
+            test_ent_correlation = {model_num: [] for model_num in range(10)}
             for model_num in range(10):
-                downstream_performance = torch.load(f"downstream/{ssl}_{pretrain}_{arch}_{model_num}_{downstream}_binary_test.pth")
-                downstream_performance = downstream_performance["brier"]
+                downstream_performance = torch.load(f"downstream/{ssl}_{pretrain}_{arch}_{model_num}_{downstream}_{downstream_task}_test.pth")
                 for stddev in ALL_STDDEVS:
-                    all_corrs = []
+                    all_brier_corrs = []
+                    all_ent_corrs = []
                     for seed in range(5):
                         uncertainty = torch.load(f"nc_uncertainty/{ssl}_{pretrain}_{arch}_{downstream}/test/ckpt_{model_num}/{method}_{stddev}_nc_{seed}.pth")
-                        all_corrs.append(kendalltau(downstream_performance, -uncertainty).statistic)
-                    test_correlation[model_num].append(all_corrs)
-            torch.save(test_correlation, f"result_cache/{ssl}_{pretrain}_{arch}_{downstream}_{method}_test_corrs.pth")
-            
-            # Train correlations and stddevs
-            train_stddevs = {model_num: [] for model_num in range(10)}
-            for model_num in range(10):
-                for stddev in ALL_STDDEVS:
-                    seed = 1234
-                    uncertainty = torch.load(f"nc_uncertainty/{ssl}_{pretrain}_{arch}_{downstream}/train/ckpt_{model_num}/{method}_{stddev}_nc_{seed}.pth")
-                    train_stddevs[model_num].append(uncertainty.std())
-            torch.save(train_stddevs, f"result_cache/{ssl}_{pretrain}_{arch}_{downstream}_{method}_train_stddevs.pth")
-
-def cache_transfer_other_methods(ssl, pretrain, arch, downstream, methods):
-    with torch.no_grad():
-        for method in methods:
-            # Test correlations
-            test_correlation = {model_num: [] for model_num in range(10)}
-            for model_num in range(10):
-                downstream_performance = torch.load(f"downstream/{ssl}_{pretrain}_{arch}_{model_num}_{downstream}_binary_test.pth")
-                downstream_performance = downstream_performance["brier"]
-                for stddev in ALL_STDDEVS:
-                    all_corrs = []
-                    for seed in range(5):
-                        uncertainty = torch.load(f"nc_uncertainty/{ssl}_{pretrain}_{arch}_{downstream}/test/ckpt_{model_num}/{method}_{stddev}_nc_{seed}.pth")
-                        all_corrs.append(kendalltau(downstream_performance, -uncertainty).statistic)
-                    test_correlation[model_num].append(all_corrs)
-            torch.save(test_correlation, f"result_cache/{ssl}_{pretrain}_{arch}_{downstream}_{method}_test_corrs.pth")
+                        all_brier_corrs.append(kendalltau(downstream_performance["brier"], -uncertainty).statistic)
+                        all_ent_corrs.append(kendalltau(downstream_performance["pred_entropy"], -uncertainty).statistic)
+                    test_brier_correlation[model_num].append(all_brier_corrs)
+                    test_ent_correlation[model_num].append(all_ent_corrs)
+            test_correlation = {"brier": test_brier_correlation, "pred_entropy": test_ent_correlation}
+            torch.save(test_correlation, f"result_cache/{ssl}_{pretrain}_{arch}_{downstream}_{downstream_task}_{method}_test_corrs.pth")
             
             # Train correlations and stddevs
             train_stddevs = {model_num: [] for model_num in range(10)}
@@ -56,30 +35,63 @@ def cache_transfer_other_methods(ssl, pretrain, arch, downstream, methods):
                     seed = 1234
                     uncertainty = torch.load(f"nc_uncertainty/{ssl}_{pretrain}_{arch}_{pretrain}/train/ckpt_{model_num}/{method}_{stddev}_nc_{seed}.pth")
                     train_stddevs[model_num].append(uncertainty.std())
-            torch.save(train_stddevs, f"result_cache/{ssl}_{pretrain}_{arch}_{pretrain}_{method}_train_stddevs.pth")
+            torch.save(train_stddevs, f"result_cache/{ssl}_{pretrain}_{arch}_{method}_train_stddevs.pth")
 
-for ssl in ["simclr", "byol", "moco"]:
+def cache_transfer_other_methods(ssl, pretrain, arch, downstream, methods, downstream_task):
+    with torch.no_grad():
+        for method in methods:
+            # Test correlations
+            test_brier_correlation = {model_num: [] for model_num in range(10)}
+            test_ent_correlation = {model_num: [] for model_num in range(10)}
+            for model_num in range(10):
+                downstream_performance = torch.load(f"downstream/{ssl}_{pretrain}_{arch}_{model_num}_{downstream}_{downstream_task}_test.pth")
+                for stddev in ALL_STDDEVS:
+                    all_brier_corrs = []
+                    all_ent_corrs = []
+                    for seed in range(5):
+                        uncertainty = torch.load(f"nc_uncertainty/{ssl}_{pretrain}_{arch}_{downstream}/test/ckpt_{model_num}/{method}_{stddev}_nc_{seed}.pth")
+                        all_brier_corrs.append(kendalltau(downstream_performance["brier"], -uncertainty).statistic)
+                        all_ent_corrs.append(kendalltau(downstream_performance["pred_entropy"], -uncertainty).statistic)
+                    test_brier_correlation[model_num].append(all_brier_corrs)
+                    test_ent_correlation[model_num].append(all_ent_corrs)
+            test_correlation = {"brier": test_brier_correlation, "pred_entropy": test_ent_correlation}
+            torch.save(test_correlation, f"result_cache/{ssl}_{pretrain}_{arch}_{downstream}_{downstream_task}_{method}_test_corrs.pth")
+            
+            # Train correlations and stddevs
+            train_stddevs = {model_num: [] for model_num in range(10)}
+            for model_num in range(10):
+                for stddev in ALL_STDDEVS:
+                    seed = 1234
+                    uncertainty = torch.load(f"nc_uncertainty/{ssl}_{pretrain}_{arch}_{pretrain}/train/ckpt_{model_num}/{method}_{stddev}_nc_{seed}.pth")
+                    train_stddevs[model_num].append(uncertainty.std())
+            torch.save(train_stddevs, f"result_cache/{ssl}_{pretrain}_{arch}_{method}_train_stddevs.pth")
+
+print("START")
+
+for downstream_task in ["binary", "multi"]:
+    for ssl in ["simclr", "byol", "moco"]:
+        for pretrain in ["cifar10", "cifar100"]:
+            for arch in ["resnet18", "resnet50"]:
+                print(ssl, pretrain, arch, pretrain, downstream_task)
+                cache_indist_other_methods(ssl, pretrain, arch, pretrain, ["random"], downstream_task)
+
+for downstream_task in ["binary", "multi"]:
+    for ssl in ["simclr", "byol", "moco"]:
+        for downstream in ["cifar10", "cifar100", "stl10"]:
+            for arch in ["resnet18", "resnet50"]:
+                print(ssl, "imagenet32", arch, downstream, downstream_task)
+                cache_transfer_other_methods(ssl, "imagenet32", arch, downstream, ["random"], downstream_task)
+
+ssl="simclr"
+for downstream_task in ["binary", "multi"]:
     for pretrain in ["cifar10", "cifar100"]:
         for arch in ["resnet18", "resnet50"]:
-            print(ssl, pretrain, arch, pretrain)
-            cache_indist_other_methods(ssl, pretrain, arch, pretrain, ["random"])
+            print(ssl, pretrain, arch, pretrain, "other_methods", downstream_task)
+            cache_indist_other_methods(ssl, pretrain, arch, pretrain, ["in_space", "ortho_space", "gradinv"], downstream_task)
 
-for ssl in ["simclr", "byol", "moco"]:
+ssl="simclr"
+for downstream_task in ["binary", "multi"]:
     for downstream in ["cifar10", "cifar100", "stl10"]:
         for arch in ["resnet18", "resnet50"]:
-            print(ssl, "imagenet32", arch, downstream)
-            cache_transfer_other_methods(ssl, "imagenet32", arch, downstream, ["random"])
-
-ssl="simclr"
-for pretrain in ["cifar10", "cifar100"]:
-    for arch in ["resnet18", "resnet50"]:
-        print(ssl, pretrain, arch, pretrain, "other_methods")
-        cache_indist_other_methods(ssl, pretrain, arch, pretrain, ["in_space", "ortho_space", "gradinv"])
-
-ssl="simclr"
-for downstream in ["cifar10", "cifar100", "stl10"]:
-    for arch in ["resnet18", "resnet50"]:
-        print(ssl, "imagenet32", arch, downstream, "other_methods")
-        cache_transfer_other_methods(ssl, "imagenet32", arch, downstream, ["in_space", "ortho_space", "gradinv"])
-
-            
+            print(ssl, "imagenet32", arch, downstream, "other_methods", downstream_task)
+            cache_transfer_other_methods(ssl, "imagenet32", arch, downstream, ["in_space", "ortho_space", "gradinv"], downstream_task)
